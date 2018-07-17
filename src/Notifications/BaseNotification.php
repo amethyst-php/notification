d6325138d9b5dbcc5cfa929803b5a01c81521809
Unknown <railken@protonmail.ch>
2018-07-17 10:33:55 +0200

    improve base notification

--- src/Notifications/BaseNotification.php
+++ src/Notifications/BaseNotification.php
@@ -12,19 +12,23 @@ class BaseNotification extends IlluminateNotification implements ShouldQueue
 
     public $event;
     public $message;
+    public $event_class;
+    public $options;
 
     /**
      * Create a new event instance.
      *
      * @param  event  $event
      * @param string $message
+     * @param array $options
      * @return void
      */
-    public function __construct($event, $message)
+    public function __construct($event, $message, array $options = [])
     {
         $this->event = $event;
         $this->message = $message;
-        $this->event->class = get_class($event);
+        $this->options = $options;
+        $this->event_class = get_class($event);
     }
 
     /**
@@ -36,7 +40,7 @@ class BaseNotification extends IlluminateNotification implements ShouldQueue
     public function via($notifiable)
     {
         return ['database'];
-    }
+    }   
 
     /**
      * Get the array representation of the notification.
@@ -50,6 +54,8 @@ class BaseNotification extends IlluminateNotification implements ShouldQueue
         return [
             'message' => $this->message,
             'event' => $this->event,
+            'event_class' => $this->event_class,
+            'options' => $this->options,
         ];
     }
 }
