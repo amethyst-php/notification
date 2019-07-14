59045e76819a7a8ee97d46d01052821c2f52bcf5
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/NotificationManager.php
+++ src/Managers/NotificationManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Notification newEntity()
+ * @method \Amethyst\Schemas\NotificationSchema getSchema()
+ * @method \Amethyst\Repositories\NotificationRepository getRepository()
+ * @method \Amethyst\Serializers\NotificationSerializer getSerializer()
+ * @method \Amethyst\Validators\NotificationValidator getValidator()
+ * @method \Amethyst\Authorizers\NotificationAuthorizer getAuthorizer()
+ */
 class NotificationManager extends Manager
 {
     use ConfigurableManager;
